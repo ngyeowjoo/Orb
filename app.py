bfe5bfe36e4f4e4cc7e9e952c6ef0a1fba0e0b25
ngyeowjoo <ngyeowjoo@gmail.com>
2026-04-30 11:21:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -72,8 +72,8 @@ body {
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: flex-start;
-    padding-top: 200px;
+    justify-content: center;
+    padding-top: 80px;
     overflow: hidden;
 }
 
